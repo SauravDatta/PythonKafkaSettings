0ce8803ead11350c664534f6ae65715b1f881a41
sdatta <sdatta@paypal.com>
2017-06-21 22:34:12 -0700

    commit

--- Mapper.py
+++ Mapper.py
@@ -15,6 +15,7 @@ def main(localproducer):
     # input comes from STDIN (standard input)
 
     data = read_input(sys.stdin)
+#Replace topictest with your topic
     localproducer.send("topictest", data.__str__())
 
 if __name__ == "__main__":
